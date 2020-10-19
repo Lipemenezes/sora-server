168f95ee5d4e403561ef500771e392d33da2352e
Felipe Menezes <felipe.menezes@ahgora.com.br>
2020-10-19 04:20:04 -0300

    feat: server v1

---
+++ server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const authMiddleware = require('./middlewares/auth');
+
+const userController = require('./controllers/user');
+
+require('dotenv-safe').config();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.get('/status', authMiddleware, (req, res, next) => {
+    res.json({ timestamp: new Date() });
+});
+
+const SERVER_PORT = 3000;
+
+app.listen(SERVER_PORT, () =>
+    console.info(`Sora server listening on port ${SERVER_PORT}`),
+);
