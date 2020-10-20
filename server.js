a9c4a5fe7016031502fd2286634d2a4e0477fe65
Felipe Menezes <felipe.menezes@ahgora.com.br>
2020-10-19 23:02:09 -0300

    fix: initialize env vars and db before app

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 'use strict';
 
+require('./config/env')();
+require('./config/db');
+
 const app = require('./config/express')();
+
 
 app.listen(process.env.SERVER_PORT, () =>
     console.info(`Sora server listening on port ${process.env.SERVER_PORT}`),
